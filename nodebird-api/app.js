cc685c09b97897488aefe0ff90e58fd4a03b33d9
seilylook <seilylook@naver.com>
2023-01-25 13:14:28 +0900

    make controller method - renderLogin, createDomain

--- nodebird-api/app.js
+++ nodebird-api/app.js
@@ -8,7 +8,6 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes');
 const { sequelize } = require('./models');
@@ -61,11 +60,11 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_DEV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
